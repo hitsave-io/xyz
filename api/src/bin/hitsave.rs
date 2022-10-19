ce8d69ef89e1529244756f83ec8402a9db05cbea
seabo <37074155+seabo@users.noreply.github.com>
2022-10-19 08:26:40 +0100

    refactor: make crate a lib, and create a bin directory for executables

--- api/src/bin/hitsave.rs
+++ api/src/bin/hitsave.rs
@@ -1,29 +1,9 @@
-#![feature(try_trait_v2)]
-#![feature(result_option_inspect)]
-
-#[macro_use]
-extern crate nonblock_logger;
-#[macro_use]
-extern crate async_trait;
-#[allow(unused_imports)]
-#[macro_use]
-extern crate validator;
-#[macro_use]
-extern crate sqlx;
-#[macro_use]
-extern crate serde;
 #[macro_use]
 extern crate lazy_static;
 
 use actix_web::{error, middleware, web, App, HttpServer, Result};
-use config::{Config, Opts};
-
-pub mod config;
-pub mod handlers;
-pub mod middlewares;
-pub mod models;
-pub mod msg_pack;
-pub mod state;
+use hitsave_api::config::{Config, Opts};
+use hitsave_api::{models, msg_pack};
 
 lazy_static! {
     pub static ref CONFIG: Config = Config::parse_from_env();
---
+++ api/src/lib.rs
@@ -0,0 +1,29 @@
+#![feature(try_trait_v2)]
+#![feature(result_option_inspect)]
+
+#[macro_use]
+extern crate nonblock_logger;
+#[macro_use]
+extern crate async_trait;
+#[allow(unused_imports)]
+#[macro_use]
+extern crate validator;
+#[macro_use]
+extern crate sqlx;
+#[macro_use]
+extern crate serde;
+#[macro_use]
+extern crate lazy_static;
+
+pub mod config;
+pub mod handlers;
+pub mod middlewares;
+pub mod models;
+pub mod msg_pack;
+pub mod state;
+
+use config::Config;
+
+lazy_static! {
+    pub static ref CONFIG: Config = Config::parse_from_env();
+}
