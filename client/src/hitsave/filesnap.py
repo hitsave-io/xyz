01ffefc17036056cb243937a471bbf0ad145a7bf
E.W.Ayers <edward.ayers@outlook.com>
2022-10-31 18:22:27 +0000

    fix: relpath is now optional on snapshots
    This is so that it doesn't spew annoying errors when you try to take a snapshot of some data that is outside workspace.

--- client/src/hitsave/filesnap.py
+++ client/src/hitsave/filesnap.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from functools import cached_property
 from genericpath import isfile
 from io import BufferedReader
-from typing import Any, List, Union
+from typing import Any, List, Optional, Union
 from hitsave.config import Config
 import os.path
 from blake3 import blake3
@@ -15,6 +15,8 @@ import pathlib
 from stat import S_IREAD, S_IRGRP, S_IROTH
 from hitsave.cloudstore import dumps as csdumps
 import logging
+
+from hitsave.util import human_size
 
 logger = logging.getLogger("hitsave")
 
@@ -30,8 +32,13 @@ class FileSnapshot:
     You can then read this file directly
     """
 
-    relpath: PurePath
-    """ original path on host machine, relative to the workspace directory."""
+    relpath: Optional[Path]
+    """ original path on host machine, relative to the workspace directory.
+
+    If it is None, then the file was snapshotted outside of the workspace directory."""
+
+    name: str
+    """ The name of the file. """
 
     digest: str
     """ BLAKE3 hash of the file """
@@ -53,26 +60,30 @@ class FileSnapshot:
         """Returns true if the file has already been cached."""
         return self.local_cache_path.exists()
 
+    @property
+    def suffix(self):
+        """The suffix extension of the file. Eg `hello.txt` has the suffix `.txt`."""
+        return Path(self.name).suffix
+
     def download(self, force=False):
         """Download the file from the cloud to the local file cache."""
         if not force and self.has_local_cache:
             # no need to download
             return
+        logger.info(f"Downloading {self.name} ({human_size(self.content_length)})")
         r = requests.request(
             "GET",
-            f"{self.url}/blob/{self.digest}",
+            f"{Config.current().cloud_url}/blob/{self.digest}",
             headers=headers,
         )
         r.raise_for_status()
         # [todo] duplicated code.
         # [todo] exclusive file lock.
         with open(self.local_cache_path, "wb") as c:
-                c.write(r.content)
+            c.write(r.content)
         # snapshot files are read only.
         # ref: https://stackoverflow.com/a/28492823/352201
-        os.chmod(
-            self.local_cache_path, S_IREAD | S_IRGRP
-        )  # [todo] what about S_IROTH?
+        os.chmod(self.local_cache_path, S_IREAD | S_IRGRP)  # [todo] what about S_IROTH?
         return
 
     def open(self, mode="t", **kwargs) -> BufferedReader:
@@ -95,10 +106,10 @@ class FileSnapshot:
             raise ValueError(f"Path {path.parent} does not exist.")
         if path.is_dir():
             logger.info(
-                f"restore_at: {path} is a directory so appending the basename of the file {self.relpath.name}."
+                f"restore_at: {path} is a directory so appending the basename of the file {self.name}."
             )
-            path = path / self.relpath.name
-        if self.relpath.suffix != path.suffix:
+            path = path / self.name
+        if self.suffix != path.suffix:
             raise ValueError(
                 f"Refusing to write to {path} since the extension name is different to extension of {self.relpath}"
             )
@@ -123,11 +134,18 @@ class FileSnapshot:
         Returns the absolute path of the file that was restored."""
         if not self.has_local_cache:
             self.download()
+        if self.relpath is None:
+            raise ValueError(f"Can't restore a snapshot without a specific path.")
         project_path = Path(project_path or Config.current().workspace_dir)
-        abspath = (project_path / self.relpath).resolve()
+        abspath = project_path / self.relpath
         assert abspath.is_relative_to(project_path)
 
         return self.restore_at(abspath, overwrite=overwrite)
+
+    def restore_safe(self):
+        if not self.has_local_cache:
+            self.download()
+        return self.local_cache_path
 
     @classmethod
     def snap(cls, path: Union[Path, str], workspace_dir=None):
@@ -142,7 +160,10 @@ class FileSnapshot:
         # [todo] do we need to lock files in case of multiprocessing? This is faff crossplatform
         if workspace_dir is None:
             workspace_dir = Config.current().workspace_dir
-        relpath = path.relative_to(workspace_dir)
+        if path.is_relative_to(workspace_dir):
+            relpath = path.relative_to(workspace_dir)
+        else:
+            relpath = None
         time = datetime.now()
         with open(path, "rb") as fd:
             block_size = 2**20
@@ -157,7 +178,11 @@ class FileSnapshot:
             digest = h.hexdigest()
             fd.seek(0)
             snap = FileSnapshot(
-                digest=digest, time=time, relpath=relpath, content_length=content_length
+                digest=digest,
+                time=time,
+                relpath=relpath,
+                content_length=content_length,
+                name=path.name,
             )
             if not snap.has_local_cache:
                 # [todo] exclusive file lock.
@@ -197,7 +222,7 @@ class FileSnapshot:
                 ),
             )
             r.raise_for_status()
-            logger.info(f"Uploaded {self.relpath} {self.content_length} bytes.")
+            logger.info(f"Uploaded {self.name} ({human_size(self.content_length)}).")
 
 
 @dataclass
@@ -207,7 +232,14 @@ class DirectorySnapshot:
     [todo]: archiving mode where it saves a directory as a .zip, .tar.gz or similar.
     """
 
-    relpath: Path
+    original_path: Path
+    """ Path on users machine when the snapshot was taken. """
+
+    relpath: Optional[Path]
+    """ Path relative to the workspace directory of the code that snapshotted this directory.
+
+    If it is none, then the snapshot was not taken on a directory that is in the workspace.
+    """
     files: List[FileSnapshot]
     digest: str
     """ All files (including files in subdirectories),  """
@@ -224,6 +256,9 @@ class DirectorySnapshot:
                     f"{path} already exists, files that are also present in the directory snapshot will be overwritten, other files will be left alone."
                 )
         for file in self.files:
+            assert (
+                file.relpath is not None
+            ), "malformed file snapshot in directory snapshot"
             filepath = path / file.relpath
             assert filepath.is_relative_to(
                 path
@@ -251,7 +286,11 @@ class DirectorySnapshot:
     def restore(self, workspace_dir=None, overwrite=True) -> Path:
         """Restores the snapshotted directory. Returns the path of the directory that was restored."""
         workspace_path = Path(workspace_dir or Config.current().workspace_dir)
-        abspath = (workspace_path / self.relpath).resolve()
+        if self.relpath is None:
+            raise ValueError(
+                "Can't restore directory without specific path. Try using restore_safe."
+            )
+        abspath = workspace_path / self.relpath
         logger.info(f"Restoring directory snapshot at {abspath}.")
         self.restore_at(abspath, overwrite=overwrite)
         return abspath
@@ -261,7 +300,13 @@ class DirectorySnapshot:
         path = Path(path).resolve()
         if workspace_dir is None:
             workspace_dir = Config.current().workspace_dir
-        relpath = path.relative_to(workspace_dir)
+        if path.is_relative_to(workspace_dir):
+            relpath = path.relative_to(workspace_dir)
+        else:
+            logger.info(
+                f"Directory {path} is outside workspace directory {workspace_dir}. Not storing a relpath."
+            )
+            relpath = None
 
         def rec(p: Path):
             for child_path in p.iterdir():
@@ -275,12 +320,13 @@ class DirectorySnapshot:
                 if child_path.is_file():
                     yield FileSnapshot.snap(child_path, workspace_dir=path)
 
-        files = sorted(rec(path), key=lambda x: x.relpath)
+        files = sorted(rec(path), key=lambda x: x.relpath or 0)
+        logger.info(f"Directory snapshot created for {len(files)} files.")
         h = blake3()
         for file in files:
             h.update(file.digest.encode())
         digest = h.hexdigest()
-        snap = cls(relpath=relpath, files=files, digest=digest)
+        snap = cls(relpath=relpath, files=files, digest=digest, original_path=path)
         return snap
 
     def upload(self):
