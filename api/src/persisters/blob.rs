05ad00ea50a7dd69e77c64a2ec0baac41b763df1
seabo <37074155+seabo@users.noreply.github.com>
2022-10-31 12:32:30 +0000

    feat: add put endpoint for BLOBs

--- api/src/persisters/blob.rs
+++ api/src/persisters/blob.rs
@@ -1,11 +1,71 @@
 use crate::handlers::blob::BlobParams;
 use crate::middlewares::api_auth::Auth;
-use crate::persisters::{s3store::StoreError, Query};
+use crate::persisters::s3store::BlobMetadata;
+use crate::persisters::{s3store::StoreError, Persist, Query};
 use crate::state::State;
 use actix_web::{
     body::BodyStream, error, http::StatusCode, web::Path, Error, HttpResponse, HttpResponseBuilder,
 };
 use blake3::{Hash, HexError};
+
+#[derive(Deserialize, Debug)]
+pub struct BlobInsert {
+    pub content_length: i64,
+    pub content_hash: String,
+}
+
+impl BlobMetadata for BlobInsert {
+    fn content_length(&self) -> i64 {
+        self.content_length
+    }
+
+    fn content_hash(&self) -> &str {
+        &self.content_hash
+    }
+}
+
+struct BlobInsertResult {
+    id: Option<i64>,
+}
+
+#[async_trait]
+impl Persist for BlobInsert {
+    type Ret = i64;
+    type Error = BlobError;
+
+    async fn persist(self, auth: Option<&Auth>, state: &State) -> Result<Self::Ret, Self::Error> {
+        let auth = auth.ok_or(BlobError::Unauthorized)?;
+
+        // Insert blob.
+        let blob_res = query_as!(
+            BlobInsertResult,
+            r#"
+            WITH s AS (
+                SELECT id 
+                FROM blobs 
+                WHERE user_id = user_from_key($1) 
+                AND content_hash = $2
+            ), i AS (
+                INSERT INTO blobs (user_id, content_hash)
+                VALUES (user_from_key($1), $2)
+                ON CONFLICT DO NOTHING
+                RETURNING id
+            )
+            SELECT id
+            FROM i UNION ALL
+            SELECT id
+            FROM s
+            "#,
+            auth.key,
+            self.content_hash,
+        )
+        .fetch_one(&state.db_conn)
+        .await?;
+
+        // TODO: get rid of the expect
+        Ok(blob_res.id.expect("should always be some"))
+    }
+}
 
 #[async_trait]
 impl Query for Path<BlobParams> {
@@ -60,6 +120,19 @@ impl From<HexError> for BlobError {
 impl From<StoreError> for BlobError {
     fn from(_: StoreError) -> Self {
         BlobError::StoreError
+    }
+}
+
+impl From<BlobError> for StoreError {
+    // TODO: this is way too hacky....
+    fn from(e: BlobError) -> Self {
+        match e {
+            BlobError::Unauthorized => StoreError::Unauthorized,
+            BlobError::InvalidHash => StoreError::InvalidHash,
+            // ...especially this!
+            BlobError::StoreError => StoreError::Unauthorized,
+            BlobError::Sqlx(e) => StoreError::Sqlx(e),
+        }
     }
 }
 
