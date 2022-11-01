6801bb5660dfe301b5a47e0805bb37965cfd70a6
seabo <37074155+seabo@users.noreply.github.com>
2022-11-01 08:54:24 +0000

    feat: add HEAD method for `/blob`

--- api/src/handlers/blob.rs
+++ api/src/handlers/blob.rs
@@ -4,7 +4,7 @@ use crate::persisters::blob::BlobInsert;
 use crate::persisters::{Persist, Query};
 use crate::state::AppState;
 use actix_web::{
-    error, get, put,
+    error, get, head, put,
     web::{self, Path},
     Error, HttpResponse,
 };
@@ -14,15 +14,29 @@ pub struct BlobParams {
     pub content_hash: String,
 }
 
+#[derive(Deserialize, Debug)]
+pub struct BlobParamsHead {
+    pub content_hash: String,
+}
+
 #[get("/{content_hash}")]
 async fn get_blob(
     content_hash: Path<BlobParams>,
     auth: Auth,
     state: AppState,
 ) -> Result<HttpResponse, Error> {
-    println!("{:?}", content_hash);
     let blob = content_hash.fetch(Some(&auth), &state).await?;
     Ok(blob)
+}
+
+#[head("/{content_hash}")]
+async fn head_blob(
+    content_hash: Path<BlobParamsHead>,
+    auth: Auth,
+    state: AppState,
+) -> Result<HttpResponse, Error> {
+    let _blob = content_hash.fetch(Some(&auth), &state).await?;
+    Ok(HttpResponse::Ok().into())
 }
 
 #[put("")]
@@ -38,5 +52,6 @@ async fn put_blob(
 
 pub fn init(cfg: &mut web::ServiceConfig) {
     cfg.service(get_blob);
+    cfg.service(head_blob);
     cfg.service(put_blob);
 }
