b17d11edbefc4f80043af78ade3721b8bb2c0d6f
seabo <37074155+seabo@users.noreply.github.com>
2022-11-15 17:53:09 +0000

    wip: change how logins work
    The GitHub OAuth system requires us to provide a redirect_url parameter, which is where the user's browser goes after they successfully completed the login flow on GitHub's side. Untili now, we've had this be `127.0.0.1:PORT` when called from the Python client. From the web application, we've used `127.0.0.1:3000/login`. This has been fine, because both of these have the same host name (`127.0.0.1`). However, in production, they can't have the same hostname, because the Python client is trying to redirect to the miniserver on the user's machine, while the web app needs to redirect to a live web page at hitsave.io. The problem is that the GitHub OAuth system will only accept one base redirect url, not multiple.
    
    To solve this, we are now doing the following, which is actually somewhat simpler. The GitHub registered `redirect_url` (also known as callback URL) will be the live hosted hitsave.io page at `https://hitsave.io/login`. This page can accept additional search params, so we are adding a `client_loopback` param, which will include the loopback URL to the miniserver. In this way, we can use the Remix website to display the pretty login page that we've always wanted, and then use a `fetch` request from the locally rendered component in a `useEffect` hook to ping the miniserver at the URL which was passed in `client_loopback`. This new approach removes the need for the Python client to communicate with the API to pass the `code` parameter through and get the user registered. All of this now happens from the NodeJS hosted web app, reusing the same code which does this for browser-initiated logins.
    
    This commit implements all of this.
    
    Note that soon, we will need to have to have two GitHub OAuth apps registered, one for production, and one for dev. The dev one will use the hostname we use locally (either 127.0.0.1, or perhaps `hitsave-local.io` if we get this working with /etc/hosts). The production one will have the callback URL set to `https://hitsave.io/login`.

--- web/src/routes/login.tsx
+++ web/src/routes/login.tsx
@@ -1,13 +1,22 @@
-import { LoaderArgs, redirect } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { useEffect } from "react";
+import { LoaderArgs, MetaFunction, redirect } from "@remix-run/node";
+import { useLoaderData, useSearchParams } from "@remix-run/react";
 
 import { API } from "~/api";
 
 export const loader = async ({ request }: LoaderArgs) => {
   const url = new URL(request.url);
   const code = url.searchParams.get("code");
-  const redirectUrl = url.searchParams.get("redirect");
+  const redirectUrl = url.searchParams.get("redirect") ?? "/dashboard";
 
+  // If a `client_loopback` params is present, it means that the login was initiated
+  // Python client, and we should respond from the user's web browser with a web fetch
+  // to the temporary localhost server running in Python, to inform that the sign in was
+  // successful.
+  const clientLoopbackUrl = url.searchParams.get("client_loopback");
+
+  // Instruct the HitSave API to attempt to retrieve the user's login details with the
+  // provided `code`.
   const res = await API.fetch(`/user/login?code=${code}`, {
     method: "post",
   });
@@ -15,16 +24,37 @@ export const loader = async ({ request }: LoaderArgs) => {
   if (res.status == 200) {
     const jwt = await res.text();
 
-    return redirect(redirectUrl ?? "/dashboard", {
-      headers: {
-        "Set-Cookie": `jwt=${jwt}; HttpOnly; Max-Age=${60 * 60 * 24 * 30};`,
-      },
-    });
+    const headers = {
+      "Set-Cookie": `jwt=${jwt}; HttpOnly; Max-Age=${60 * 60 * 24 * 30};`,
+    };
+
+    if (!clientLoopbackUrl) {
+      // This is an attempt to access a protected page - we don't want to waste any time
+      // showing a 'successfully logged in page', so just redirect to their desired URL.
+      return redirect(redirectUrl, { headers });
+    }
+
+    // If we get here, then the login was initiated by the Python client. We would like to
+    // display a success message, and send a message to the local Python mini-server from
+    // the browser.
+    return new Response(jwt, { headers });
   } else {
     throw new Error("Unable to log you in.");
   }
 };
 
 export default function LoginPage() {
-  useLoaderData<typeof loader>();
+  const jwt = useLoaderData<typeof loader>();
+  console.log(jwt);
+  const [searchParams] = useSearchParams();
+  const clientLoopbackUrl = searchParams?.get("client_loopback");
+
+  useEffect(() => {
+    if (clientLoopbackUrl) {
+      const url = new URL(`${clientLoopbackUrl}?jwt=${jwt}`);
+      fetch(url, { method: "get" });
+    }
+  });
+
+  return <>{clientLoopbackUrl && <>Successfully logged in.</>}</>;
 }
