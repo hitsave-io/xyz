200ba5251edf29a5b2bee3285cf4926b7802c984
seabo <37074155+seabo@users.noreply.github.com>
2022-11-17 16:38:45 +0000

    ci: try this

--- deploy/aws/scripts/after-install.sh
+++ deploy/aws/scripts/after-install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# Log the outputs of this script to after-install.log
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>after-install.log 2>&1
+
+# Everything from here will be logged.
 set -xe
 cd /home/ec2-user
 
@@ -9,7 +16,7 @@ aws s3 cp s3://hitsave-prod-deploy-archive/build.tar.gz build.tar.gz
 tar -xzf build.tar.gz
 rm build.tar.gz
 
-# Move the files to their correct locations. Since these directions may not be 
+# Move the files to their correct locations. Since these directories may not be 
 # empty, we use rsync.
 rsync -a -v api/* xyz/api/
 rsync -a -v web/* xyz/web/
@@ -25,7 +32,7 @@ cd xyz/deploy
 ln -s ~/.secrets .secrets
 
 # Build latest docker compose images
-docker compose build
+docker compose build --no-cache
 
 # Launch application containers, using production version of docker-compose.yml
 docker compose -f docker-compose.yml -f docker-compose.production.yml up -d
--- deploy/aws/scripts/before-install.sh
+++ deploy/aws/scripts/before-install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# Log the outputs of this script to after-install.log
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>before-install.log 2>&1
+
+# Everything from here will be logged.
 set -xe
 
 # Delete the old directory as needed.
