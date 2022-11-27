a1e0f47bb2a02bca0039bf43b8876c72ebc4514a
seabo <37074155+seabo@users.noreply.github.com>
2022-11-27 16:02:12 +0000

    feat: add geo features to analytics

--- web/src/analytics/index.ts
+++ web/src/analytics/index.ts
@@ -1,11 +1,11 @@
 import { RequestHandler } from "express";
+import geoip from "geoip-lite";
+
 import { db } from "../db";
 import { parseCookie } from "../utils/cookie";
 
 // Express middleware for logging certain request data to the database.
 export const registerPageload: RequestHandler = async (req, _res, next) => {
-  console.log(req);
-
   const route = req.path;
 
   // we only want to record pageloads, not every request (which would include
@@ -15,23 +15,30 @@ export const registerPageload: RequestHandler = async (req, _res, next) => {
     return;
   }
 
-  const cookie = req.headers["cookie"] || "";
+  const cookie = req.headers["cookie"] || null;
   const referer = req.headers["referer"] || null;
   const useragent = req.headers["user-agent"];
-  const jwt = parseCookie(cookie)["jwt"];
+  const jwt = cookie ? parseCookie(cookie)["jwt"] : null;
 
-  const ip =
-    (req.headers["x-forwarded-for"] || "")[0] ||
-    req.socket?.remoteAddress ||
-    req.ip;
+  let forwardedFor = req.headers["x-forwarded-for"] || null;
+  if (Array.isArray(forwardedFor)) {
+    forwardedFor = forwardedFor.join(",");
+  }
+  let ip = forwardedFor || req.socket?.remoteAddress || req.ip;
 
-  console.log("req ip address: ", ip);
+  const geo = geoip.lookup(ip);
+  const country = geo?.country || null;
+  const region = geo?.region || null;
+
+  // Slice the city to 32 chars, just in case it's really long for some reason.
+  // We are using a VARCHAR(32) in Postgres, so it will crash otherwise.
+  const city = geo?.city.slice(0, 32) || null;
 
   const [_results, _metadata] = await db.query(
-    `INSERT INTO pageloads (route, user_agent, referer, ip, auth)
-    VALUES (?, ?, ?, ?, ?)`,
+    `INSERT INTO pageloads (route, user_agent, referer, ip, country, region, city, auth)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
     {
-      replacements: [route, useragent, referer, ip, jwt],
+      replacements: [route, useragent, referer, ip, country, region, city, jwt],
     }
   );
 
